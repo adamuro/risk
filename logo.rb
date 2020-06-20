580d65d3150f8fa5f57e6e7b71eb5ae9edb0062e
adamuro <horosporo@gmail.com>
2020-06-20 03:09:33 +0200

    Deleted logo.rb

--- logo.rb
+++
@@ -1,9 +0,0 @@
-class Logo
-  def initialize()
-    @img = Gosu::Image.new('images/main_menu/logo.png')
-  end
-
-  def draw(width, y = 0)
-    @img.draw(width / 2 - @img.width / 2, y, 1)
-  end
-end
